f02871adc9d4c77ed75bf3fa6b6bc0782b294aa3
Arthur Xavier <arthur.xavierx@gmail.com>
2018-09-04 09:40:53 -0300

    Add the CofreeT comonad transformer

---
+++ src/Control/Comonad/Cofree/Trans.purs
@@ -0,0 +1,56 @@
+module Control.Comonad.Cofree.Trans
+  ( CofreeT
+  , cofreeT
+  , runCofreeT
+  , coresume
+  , unfoldCofreeT
+  ) where
+
+import Prelude
+
+import Control.Comonad (class Comonad, extract)
+import Control.Comonad.Trans.Class (class ComonadTrans)
+import Control.Extend (class Extend, extend)
+import Data.Bifunctor (bimap)
+import Data.Identity (Identity)
+import Data.Lazy (Lazy, defer, force)
+import Data.Tuple (Tuple(..), fst, snd)
+
+type CofreeF f a b = Tuple a (Lazy (f b))
+
+newtype CofreeT f w a = CofreeT (w (CofreeF f a (CofreeT f w a)))
+
+type Cofree f = CofreeT f Identity
+
+cofreeT :: forall f w a. Comonad w => a -> (Unit -> w (f (CofreeT f w a))) -> CofreeT f w a
+cofreeT a lw = CofreeT (extend (\w -> Tuple a (defer \_ -> extract w)) (lw unit))
+
+runCofreeT :: forall f w a. CofreeT f w a -> w (CofreeF f a (CofreeT f w a))
+runCofreeT (CofreeT w) = w
+
+coresume :: forall f w a. Comonad w => CofreeT f w a -> w (Tuple a (f (CofreeT f w a)))
+coresume (CofreeT w) = w <#> \(Tuple a as) -> Tuple a (force as)
+
+unfoldCofreeT :: forall f w s a. Functor f => Comonad w => (s -> w (Tuple a (f s))) -> s -> CofreeT f w a
+unfoldCofreeT f s = CofreeT $ f s <#> map (\next -> defer \_ -> unfoldCofreeT f <$> next)
+
+instance eqCofreeT :: Eq (w (CofreeF f a (CofreeT f w a))) => Eq (CofreeT f w a) where
+  eq = eq
+
+instance ordCofreeT :: Ord (w (CofreeF f a (CofreeT f w a))) => Ord (CofreeT f w a) where
+  compare = compare
+
+instance functorCofreeT :: (Functor f, Functor w) => Functor (CofreeT f w) where
+  map f (CofreeT w) = CofreeT $ map (bimap f (map (map (map f)))) w
+
+instance extendCofreeT :: (Functor f, Comonad w) => Extend (CofreeT f w) where
+  extend f =
+    CofreeT
+    <<< extend (\w -> Tuple (f (CofreeT w)) (map (extend f) <$> snd (extract w)))
+    <<< runCofreeT
+
+instance comonadCofreeT :: (Functor f, Comonad w) => Comonad (CofreeT f w) where
+  extract (CofreeT w) = fst (extract w)
+
+instance comonadTransCofreeT :: ComonadTrans (CofreeT f) where
+  lower = map fst <<< runCofreeT
